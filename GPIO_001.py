b101b2364768fb0cdc49fa44477a11427465ddce
fogim88et <franck.ouion@univ-lorraine.fr>
2016-12-05 07:45:49 +0000

    modif ligne commentaire

--- GPIO_001.py
+++ GPIO_001.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO  #librairie
-GPIO.setmode(GPIO.BCM) # mode identification
+GPIO.setmode(GPIO.BCM) # mode identification des broches
 configuration=GPIO.getmode()
 print(configuration)
 GPIO.setup(5,GPIO.OUT)
